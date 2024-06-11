0fe359023a7474d6a068e3f9ccb8a2dd44f88777
Błażej Sowa <bsowa123@gmail.com>
2024-06-11 10:49:27 +0200

    Nixify waybar scripts

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
-pkgs:
-{
-  # example = pkgs.callPackage ./example { };
+pkgs: {
+  waybar-scripts = pkgs.callPackage ./waybar-scripts { };
 }
