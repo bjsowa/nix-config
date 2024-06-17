83daec08c9ed913bef88ee129ed6a5522b4342e2
Błażej Sowa <bsowa123@gmail.com>
2024-06-17 13:40:55 +0200

    Remove unused pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
-  hypr-scripts = pkgs.callPackage ./hypr-scripts { };
   my-nixos-scripts = pkgs.callPackage ./my-nixos-scripts { };
-  waybar-scripts = pkgs.callPackage ./waybar-scripts { };
 }
