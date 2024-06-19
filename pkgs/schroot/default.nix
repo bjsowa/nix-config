b36456039235a4345e0463cd9300ddf024f4ca49
Błażej Sowa <bsowa123@gmail.com>
2024-06-19 18:09:29 +0200

    Working schroot package

--- pkgs/schroot/default.nix
+++ pkgs/schroot/default.nix
@@ -1,6 +1,10 @@
-{ lib, stdenv, fetchurl, boost, cmake, gettext, mandoc, perl, perlPackages }:
+{ lib, stdenv, fetchurl, pkgs }:
 
-stdenv.mkDerivation rec {
+let
+  scripts-bin-path = with pkgs;
+    lib.makeBinPath [ coreutils getent gnugrep gnused gnutar util-linux ];
+
+in stdenv.mkDerivation rec {
   pname = "schroot";
   version = "1.6.13";
 
@@ -10,17 +14,36 @@ stdenv.mkDerivation rec {
     hash = "sha256-wF1qG7AhDUAeZSLu4sRl4LQ8bJj3EB1nH56e+Is6zPU=";
   };
 
-  patches = map fetchurl (import ./debian-patches.nix)
-    ++ [ ./no-setuid.patch ./no-pam-service.patch ./no-default-config.patch ];
+  patches = map fetchurl (import ./debian-patches.nix) ++ [
+    ./no-setuid.patch
+    ./no-pam-service.patch
+    ./no-default-config.patch
+    ./fix-absolute-paths.patch
+  ];
 
-  nativeBuildInputs = [ cmake ];
+  nativeBuildInputs = with pkgs; [ cmake findutils makeWrapper ];
 
-  buildInputs = [ boost gettext mandoc perl perlPackages.Po4a ];
+  buildInputs = with pkgs; [ boost gettext mandoc perl perlPackages.Po4a ];
 
   cmakeFlags = [
+    "-DCMAKE_INSTALL_LOCALSTATEDIR=/var"
     "-DSCHROOT_SYSCONF_DIR=/etc/schroot"
     "-DSCHROOT_CONF_SETUP_D=${placeholder "out"}/etc/schroot/setup.d"
   ];
+
+  postPatch = ''
+    substituteInPlace bin/schroot-mount/schroot-mount-main.cc \
+      --replace-fail "/bin/mount" "${pkgs.util-linux}/bin/mount"
+  '';
+
+  postFixup = ''
+    mkdir $out/etc/schroot/setup.d.wrapped
+    cd $out/etc/schroot/setup.d
+    find * -type f | while read -r file; do
+      mv "$file" $out/etc/schroot/setup.d.wrapped
+      makeWrapper "$out/etc/schroot/setup.d.wrapped/$file" "$file" --set PATH ${scripts-bin-path}
+    done
+  '';
 
   meta = with lib; {
     description = "Lightweight virtualisation tool";
