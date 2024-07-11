b38353f9307182fd917c65a61d694781dc36ff60
Błażej Sowa <bsowa123@gmail.com>
2024-07-11 14:36:36 +0200

    Set default internet browser to vivaldi

--- nixos/blazej-legion/configuration.nix
+++ nixos/blazej-legion/configuration.nix
@@ -472,4 +472,12 @@
       };
     };
   };
+
+  xdg.mime.defaultApplications = {
+    "text/html" = "vivaldi-stable.desktop";
+    "x-scheme-handler/http" = "vivaldi-stable.desktop";
+    "x-scheme-handler/https" = "vivaldi-stable.desktop";
+    "x-scheme-handler/about" = "vivaldi-stable.desktop";
+    "x-scheme-handler/unknown" = "vivaldi-stable.desktop";
+  };
 }
