f110f24a058091198c25bb4b93ba9e713bcb134e
Błażej Sowa <bsowa123@gmail.com>
2024-07-22 15:39:32 +0200

    Add fix-binary bit to binfmt registration

--- nixos/blazej-legion/configuration.nix
+++ nixos/blazej-legion/configuration.nix
@@ -13,7 +13,10 @@
   ];
 
   boot = {
-    binfmt.emulatedSystems = [ "aarch64-linux" ];
+    binfmt = {
+      emulatedSystems = [ "aarch64-linux" ];
+      registrations = { "aarch64-linux" = { fixBinary = true; }; };
+    };
 
     blacklistedKernelModules = [ "ideapad_laptop" ];
 
