e08459d58502e555a7a76f9fd18da7bf620b8080
Błażej Sowa <bsowa123@gmail.com>
2024-11-02 15:13:01 +0100

    Fix double notification sound when changing volume

--- pkgs/my-nixos-scripts/src/hypr/volume.sh
+++ pkgs/my-nixos-scripts/src/hypr/volume.sh
@@ -4,14 +4,12 @@ down() {
   pamixer -d 2
   volume=$(pamixer --get-volume) 
   dunstify -a "VOLUME" "Decreasing to $volume%" -h int:value:"$volume" -i audio-volume-low-symbolic -r 2593 -u normal
-  canberra-gtk-play -i audio-volume-change -d "changevolume"
 }
 
 up() {
   pamixer -i 2
   volume=$(pamixer --get-volume)
   dunstify -a "VOLUME" "Increasing to $volume%" -h int:value:"$volume" -i audio-volume-high-symbolic -r 2593 -u normal
-  canberra-gtk-play -i audio-volume-change -d "changevolume"
 }
 
 mute() {
