ff8cfad2e7eeef71b16ba32fc5b3979bdce042ec
Błażej Sowa <bsowa123@gmail.com>
2024-11-04 11:45:33 +0100

    Remove caprine overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,36 +6,6 @@
     };
 
   modifications = final: prev: {
-    caprine-bin = prev.appimageTools.wrapType2 rec {
-      pname = "caprine";
-      version = "2.60.1";
-      src = prev.fetchurl {
-        url =
-          "https://github.com/sindresorhus/caprine/releases/download/v${version}/Caprine-${version}.AppImage";
-        name = "Caprine-${version}.AppImage";
-        sha256 = "sha256-qeJZ3DoeI46/JUJq0jA6HiefzI8AlxdbTuDhTiLmZ8w=";
-      };
-
-      extracted =
-        prev.appimageTools.extractType2 { inherit pname version src; };
-
-      passthru = { inherit pname version src; };
-
-      profile = ''
-        export LC_ALL=C.UTF-8
-      '';
-
-      extraInstallCommands = ''
-        mkdir -p $out/share
-        "${prev.xorg.lndir}/bin/lndir" -silent "${extracted}/usr/share" "$out/share"
-        ln -s ${extracted}/caprine.png $out/share/icons/caprine.png
-        mkdir $out/share/applications
-        cp ${extracted}/caprine.desktop $out/share/applications/
-        substituteInPlace $out/share/applications/caprine.desktop \
-            --replace AppRun caprine
-      '';
-    };
-
     flameshot = prev.flameshot.overrideAttrs (old: {
       src = prev.fetchFromGitHub {
         owner = "flameshot-org";
