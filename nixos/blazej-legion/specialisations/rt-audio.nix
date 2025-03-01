84f12c067ab1e2521b6307c7598aee63ca74f534
Błażej Sowa <bsowa123@gmail.com>
2025-03-01 14:28:07 +0100

    Update rt-audio specialisation

--- nixos/blazej-legion/specialisations/rt-audio.nix
+++ nixos/blazej-legion/specialisations/rt-audio.nix
@@ -1,17 +1,21 @@
-{ inputs, outputs, lib, config, pkgs, ... }: {
-  imports = [ inputs.musnix.nixosModules.musnix ];
+{ inputs, pkgs, lib, ... }: {
+  imports = [
+    inputs.musnix.nixosModules.musnix
+    # can't use nvidia with realtime kernel
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
+  ];
 
-  boot = {
-    kernelPackages = pkgs.master.linuxPackages_6_6;
-    kernelParams = lib.mkAfter [ "mitigations=off" ];
-  };
+  boot.kernelParams = lib.mkAfter [ "mitigations=off" ];
 
   musnix = {
     enable = true;
 
     rtcqs.enable = true;
     soundcardPciId = "06:00.6";
-  };
 
-  hardware.bluetooth.enable = lib.mkForce false;
+    kernel = {
+      realtime = true;
+      packages = pkgs.linuxPackages_6_11_rt;
+    };
+  };
 }
