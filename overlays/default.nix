cc820944961132a4287ebef00cb2449f01008cf9
Błażej Sowa <bsowa123@gmail.com>
2025-04-30 22:36:45 +0200

    Remove custom mesa

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,21 +3,6 @@
 
   modifications = final: prev: {
     xwayland = inputs.nixpkgs-unstable.legacyPackages.${final.system}.xwayland;
-
-    mymesa = inputs.nixpkgs-master.legacyPackages.${final.system}.mesa.overrideAttrs (old: {
-      src = prev.fetchFromGitLab {
-        domain = "gitlab.freedesktop.org";
-        owner = "mesa";
-        repo = "mesa";
-        rev = "b74a6e05bd6d9bcbe1367695cb8c7a8c61f93ba8";
-        hash = "sha256-JVDjZ4f6KJqb3eV3fekdbZdKauep+j0fa+SKv08iwyc=";
-      };
-
-      patches = [
-        ./opencl.patch
-        ./system-gbm.patch
-      ];
-    });
   };
 
   unstable-packages = final: _prev: {
