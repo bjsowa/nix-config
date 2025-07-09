bf246e45a0e13ade19f7de817d8e207d806a1bf2
Błażej Sowa <bsowa123@gmail.com>
2025-07-09 17:51:59 +0200

    Update nix package manager config

--- nixos/blazej-legion/configuration.nix
+++ nixos/blazej-legion/configuration.nix
@@ -365,10 +365,7 @@
   in {
     settings = {
       experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
       flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
-      nix-path = config.nix.nixPath;
       trusted-users = [ "blazej" ];
     };
     # package = pkgs.unstable.nix;
@@ -392,7 +389,7 @@
     # Opinionated: disable channels
     channel.enable = false;
 
-    # Opinionated: make flake registry and nix path match flake inputs
+    # Make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
