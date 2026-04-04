da1aeaa1172ac0e8ab693285fc00342401ed388f
Błażej Sowa <bsowa123@gmail.com>
2026-04-04 23:04:18 +0200

    Update hyprland

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
     # hyprland
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.54.1";
+      url = "github:hyprwm/Hyprland?ref=v0.54.2";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
